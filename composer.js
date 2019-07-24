744dffa1738ca0a6c3957cee816e8835ff21eed6
Cong Tru <hocongtru95@gmail.com>
2019-07-24 15:33:42 +0700

    create package

---
+++ composer.js
@@ -0,0 +1,21 @@
+{
+    "name": "123job/captcha",
+    "description": "Package captcha",
+    "type": "php",
+    "license": "aaa",
+    "authors": [
+    {
+        "name": "Cong Tru",
+        "email": "hocongtru95@gmail.com"
+    }
+],
+    "minimum-stability": "dev",
+    "require": {
+    "php": ">=5.3.0",
+},
+    "autoload": {
+    "psr-4": {
+        "123job\\recaptchalib\\": "src/"
+    }
+}
+}
